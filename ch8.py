1856ef327a8bd3f61fabbb2deff7f3693b18f3ce
MoonAndEye <atimis19@gmail.com>
2016-03-08 23:30:43 +0900

    範例完成
    完成gui範例

--- ch8.py
+++ ch8.py
@@ -7,6 +7,20 @@ Created on Tue Mar  8 23:08:35 2016
 
 from tkinter import *
 
+def save_data():
+    fileD = open("delieveries.txt", "a")
+    fileD.write("Depot:\n")
+    fileD.write("%s\n" % depot.get())
+    fileD.write("Description:")
+    fileD.write("%s\n" % description.get())
+    fileD.write("Address:")
+    fileD.write("%s\n" % address.get("1.0", END))
+    fileD.close
+    depot.delete(0, END)
+    description.delete(0, END)
+    address.delete("1.0", END)
+    
+    
 app = Tk()
 app.title("Head-Ex Deliveries")
 Label(app, text = "Depot:").pack()
@@ -20,5 +34,7 @@ description.pack()
 Label(app, text = "Address:").pack()
 address = Text(app)
 address.pack()
+
+Button(app, text = "Save", command = save_data).pack()
 
 app.mainloop()
