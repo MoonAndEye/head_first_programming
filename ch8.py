0cc056cc45a276e433bcae3b68845e4a530916fd
MoonAndEye <atimis19@gmail.com>
2016-03-09 20:27:55 +0900

    完成了 radiobutton

--- ch8.py
+++ ch8.py
@@ -16,7 +16,7 @@ def save_data():
     fileD.write("Address:\n")
     fileD.write("%s\n" % address.get("1.0", END))
     fileD.close
-    depot.delete(0, END)
+    depot.set(None)
     description.delete(0, END)
     address.delete("1.0", END)
     
@@ -24,9 +24,18 @@ def save_data():
 app = Tk()
 app.title("Head-Ex Deliveries")
 Label(app, text = "Depot:").pack()
-depot = Entry(app)
-depot.pack()
- 
+#depot = Entry(app)
+#depot.pack()
+
+depot = StringVar()
+depot.set(None)
+
+
+Radiobutton(app, text = "Cambridge, MA", value = "Cambridge, MA", variable = depot).pack()
+Radiobutton(app, text = "Cambridge, UK", value = "Cambridge, UK", variable = depot).pack()
+Radiobutton(app, text = "Seattle, WA", value = "Seattle, WA", variable = depot).pack()
+
+
 Label(app, text = "Description:").pack()
 description = Entry(app)
 description.pack()
